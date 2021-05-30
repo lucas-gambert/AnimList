2c55c95cd9ca6de4251c60077c7dab7c2596015d
lgamb <lgambert91310@gmail.com>
2021-05-30 17:36:57 +0200

    fix

--- app/src/main/java/com/example/appliesiea/SplashActivity.kt
+++ app/src/main/java/com/example/appliesiea/SplashActivity.kt
@@ -1,16 +1,14 @@
-    package com.example.appliesiea.presentation.list
+    package com.example.appliesiea
 
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
-import com.example.appliesiea.MainActivity
-import com.example.appliesiea.R
 
     class SplashActivity : AppCompatActivity() {
 
         // This is the loading time of the splash screen
-        private val SPLASH_TIME_OUT:Long = 2000 // 1 sec
+        private val SPLASH_TIME_OUT:Long = 2500 // 1 sec
         override fun onCreate(savedInstanceState: Bundle?) {
             super.onCreate(savedInstanceState)
             setContentView(R.layout.activity_splash)
